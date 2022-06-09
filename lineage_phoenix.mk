192c17c849e483fb29f7608cc9aae4b1f74378b3
SharmagRit <sharmagrit04@gmail.com>
2022-06-09 20:08:15 +0530

    phoenix: Switch to June 2022 Raven fingerprint Signed-off-by: SharmagRit <sharmagrit04@gmail.com>

--- lineage_phoenix.mk
+++ lineage_phoenix.mk
@@ -24,10 +24,11 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 WITH_GAPPS := true
 
+# Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220405.004 8233519 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220605.009.B1 8650216 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
