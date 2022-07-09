35a1f40056147325ac2db60009f979bbab3d3fa2
SharmagRit <sharmagrit04@gmail.com>
2022-07-09 10:46:21 +0530

    phoenix: Import Lineage OS overlays

--- device.mk
+++ device.mk
@@ -367,6 +367,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage \
     CarrierConfigOverlayPhoenix \
     DialerOverlayPhoenix \
     FrameworksResOverlayPhoenix \
